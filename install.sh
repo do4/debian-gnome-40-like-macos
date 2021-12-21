fd76b322cebcfdf87ff6a966f7f6e16d2d823996
Do4, Inc <do4@users.noreply.github.com>
2021-12-20 21:56:40 -0700

    add -o

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ mkdir .icon
 mkdir .theme
 
 cd Download
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/WhiteSur-dark.tar.xz?raw=true
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/BigSur-black.tar.xz?raw=true
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/McMojave-cursors.tar.xz?raw=true
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/mcOS-BS-Dark.tar.gz?raw=true
+wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/WhiteSur-dark.tar.xz?raw=true -o WhiteSur-dark.tar.xz
+wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/BigSur-black.tar.xz?raw=true -o BigSur-black.tar.xz
+wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/McMojave-cursors.tar.xz?raw=true -o McMojave-cursors.tar.xz
+wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/mcOS-BS-Dark.tar.gz?raw=true -o mcOS-BS-Dark.tar.gz
 tar -xf WhiteSur-dark.tar.xz
 tar -xf BigSur-black.tar.xz
 tar -xf McMojave-cursors.tar.xz
