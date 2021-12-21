582bfcbb2ae847f96ea73f7e52ec8ed5436a0098
Do4, Inc <do4@users.noreply.github.com>
2021-12-20 22:07:04 -0700

    getFolder()

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@ cd ~
 mkdir .icon
 mkdir .theme
 
-cd Download
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/WhiteSur-dark.tar.xz?raw=true -o WhiteSur-dark.tar.xz
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/BigSur-black.tar.xz?raw=true -o BigSur-black.tar.xz
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/McMojave-cursors.tar.xz?raw=true -o McMojave-cursors.tar.xz
-wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/mcOS-BS-Dark.tar.gz?raw=true -o mcOS-BS-Dark.tar.gz
-tar -xf WhiteSur-dark.tar.xz
-tar -xf BigSur-black.tar.xz
-tar -xf McMojave-cursors.tar.xz
-tar -xf mcOS-BS-Dark.tar.gz
+getFolder () {
+  wget https://github.com/do4/debian-gnome-40-like-macos/blob/main/${1}.tar.xz?raw=true
+  mv "${1}.tar.xz?raw=true" ${1}.tar.xz
+  tar -xf ${1}.tar.xz
+  rm ${1}.tar.xz
+}
+
+getFolder WhiteSur-dark
+getFolder BigSur-black
+getFolder McMojave-cursors
+getFolder mcOS-BS-Dark
 
 sudo apt install gnome-tweak-tool
 sudo apt install cairo-dock
