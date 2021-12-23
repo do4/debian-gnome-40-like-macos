4cdc700468801a8c3d77a591844d54c46e0248db
Do4, Inc <do4@users.noreply.github.com>
2021-12-23 13:51:29 -0700

    if -d /usr/share/themes/

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 cd ~
-#mkdir .icon
-#mkdir .theme
 
 getFolder () {
   wget https://raw.githubusercontent.com/do4/debian-gnome-40-like-macos/main/main/${1}.tar.xz
@@ -9,18 +7,20 @@ getFolder () {
 }
 
 getFolder WhiteSur-dark
-sudo mv WhiteSur-dark /usr/share/themes/
-
 getFolder BigSur-black
-sudo mv BigSur-black /usr/share/themes/
-
 getFolder McMojave-cursors
-sudo mv McMojave-cursors /usr/share/icons/
-
 getFolder mcOS-BS-Dark
-sudo mv mcOS-BS-Dark /usr/share/themes/
 
-[ -d BigSur-black-dark ] && sudo mv BigSur-black-dark /usr/share/themes/
+if [ -d /usr/share/themes/ ]; then
+  sudo mv WhiteSur-dark /usr/share/themes/
+  sudo mv BigSur-black /usr/share/themes/
+  sudo mv mcOS-BS-Dark /usr/share/themes/
+  [ -d BigSur-black-dark ] && sudo mv BigSur-black-dark /usr/share/themes/
+fi
+
+if [ -d /usr/share/icons/ ]; then
+  sudo mv McMojave-cursors /usr/share/icons/
+fi
 
 sudo apt install gnome-tweak-tool
 sudo apt install cairo-dock
